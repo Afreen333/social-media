3ac95a5a10cffe3057d88d22a274e15fff2ec18e
Afreen333 <87018815+Afreen333@users.noreply.github.com>
2022-05-18 22:42:45 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import Login from './components/Login';
+import Posts from './components/Posts';
+import Profile from './components/Profile';
+import Home from './components/Home';
+import Signup from './components/Signup';
 import './App.css';
+import Editprofile from './components/Editprofile';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/posts" element={<Posts />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+      <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/editprofile" element={<Editprofile/>}></Route>
+      <Route path="/" element={<Home/>}></Route>
+      
+    </Routes>
+      </BrowserRouter>
+    
   );
 }
 
