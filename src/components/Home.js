c815339cad6293e4247ae66f7f2dc597a7ba5dd5
Afreen333 <87018815+Afreen333@users.noreply.github.com>
2022-05-19 13:28:42 +0530

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Profile from './Profile';
 
-export default function Home() {
+import Posts from './Posts';
+
+export default function ButtonAppBar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Connect
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    <Profile/>
+    <h2>FEED</h2>
+    <Posts/>
+    <Posts/>
+    <Posts/>
+    </>
+  );
 }
