1bb43d48f31dcf626b7f439dc533ebef5bbb95ee
Afreen333 <87018815+Afreen333@users.noreply.github.com>
2022-05-19 11:19:34 +0530

    Add files via upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-export default class login extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+export default function BasicTextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    > 
+      <h2>Login</h2>
+      <TextField id="outlined-basic" label="Username or Email" variant="outlined" /><br></br>
+      <TextField id="outlined-basic" label="Password" variant="outlined" /><br></br>
+      
+      <Button variant="contained">login</Button>
+    </Box>
+  );
 }
